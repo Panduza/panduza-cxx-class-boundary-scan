dbeb543148b433194ebf0767c21df612630d43f6
Valentin <valentin.qiu@elsys-design.com>
2022-08-25 16:56:34 +0200

    rename entrypoint class name

--- entrypoint.hxx
+++ entrypoint.hxx
@@ -1,11 +1,12 @@
-#ifndef _ENTRYPOINT_
-#define _ENTRYPOINT_
+#ifndef _PLUGINENTRYPOINT_
+#define _PLUGINENTRYPOINT_
 #pragma once
 
 #include <boost/config.hpp>
 #include "/panduza-cxx-platform/src/meta_driver.hxx"
 
-class BOOST_SYMBOL_VISIBLE Entrypoint
+/// Entrypoint of the plugin template
+class BOOST_SYMBOL_VISIBLE PluginEntrypoint
 {
 public:
     virtual std::map<std::string, MetaDriverFactory *> getInformationAndFactory() = 0;
