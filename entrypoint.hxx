b608bfd63b46bb62cd35fd26fcb4ed2825853c76
Valentin <valentin.qiu@elsys-design.com>
2022-09-06 16:00:00 +0200

    edit documentation

--- entrypoint.hxx
+++ entrypoint.hxx
@@ -5,11 +5,11 @@
 #include <boost/config.hpp>
 #include "/panduza-cxx-platform/src/meta_driver.hxx"
 
-/// Entrypoint of the plugin template
+/// @brief Entrypoint for the boundary scan plugin
 class BOOST_SYMBOL_VISIBLE PluginEntrypoint
 {
 public:
-    /// virtual function of the function that will return the driver name and factory of the plugin
+    /// @brief return the driver name and factory of the plugin @return Boundary Scan Factory information as a Map data
     virtual std::map<std::string, MetaDriverFactory *> getInformationAndFactory() = 0;
 };
 
