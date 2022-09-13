3b3a639e70dd49e0b0702cfa2a83a7fbd6fe73e5
Valentin <valentin.qiu@elsys-design.com>
2022-09-13 10:15:23 +0200

    remove useless file

--- examples/elsys-board-arty-s7/start-platform.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-cd /panduza-cxx-platform/build
-
-sudo ./panduza-cxx-platform -v 9
-
-# sudo mkdir /etc/panduza
-# sudo cp /home/builder/panduza-cxx-class-boundary-scan/examples/elsys-board-arty-s7/panduza/tree.json /etc/panduza
