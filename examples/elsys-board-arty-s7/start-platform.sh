a56f510fa6f282e01cc69e2b4bb900f34f34fae1
Valentin <valentin.qiu@elsys-design.com>
2022-09-13 10:14:11 +0200

    adding script and changing their permissions

---
+++ examples/elsys-board-arty-s7/start-platform.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+cd /panduza-cxx-platform/build
+
+sudo ./panduza-cxx-platform -v 9
+
+# sudo mkdir /etc/panduza
+# sudo cp /home/builder/panduza-cxx-class-boundary-scan/examples/elsys-board-arty-s7/panduza/tree.json /etc/panduza
