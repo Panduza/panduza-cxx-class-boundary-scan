8f02dffb9f974a1715404dcd46f063efd8d817b7
Valentin <valentin.qiu@elsys-design.com>
2022-10-06 16:41:12 +0200

    moving error exiting

--- plugins/ft2232/src/ft2232_boundary_scan/jtag_manager.cxx
+++ plugins/ft2232/src/ft2232_boundary_scan/jtag_manager.cxx
@@ -176,6 +176,12 @@ int JtagFT2232::getAvailableProbes(jtag_core *mJc, std::string probe_name)
     std::string good_probe = "Probe: " + probe_name;
     int nb_of_probes = jtagcore_get_number_of_probes(mJc, 0);
 
+    if (nb_of_probes == 0)
+    {
+        LOG_F(ERROR, "No probes detected");
+        exit(0);
+    }
+
     while (i < nb_of_probes)
     {
         jtagcore_get_probe_name(mJc, PROBE_ID(0, i), tempstring);
@@ -195,13 +201,7 @@ int JtagFT2232::getAvailableProbes(jtag_core *mJc, std::string probe_name)
 
     if (good_id == -1)
     {
-        LOG_F(ERROR, "Probe %s not found", good_probe.c_str());
-        exit(0);
-    }
-
-    if (nb_of_probes == 0)
-    {
-        LOG_F(ERROR, "No probes detected");
+        LOG_F(ERROR, "%s not found", good_probe.c_str());
         exit(0);
     }
 
